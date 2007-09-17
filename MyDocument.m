d5434e4a6d7cb7115b5ffc771be39e7c311c955e
Tetsuro Kurita <tkurita@mac.com>
2007-09-17 09:14:06 +0000

    [QuickDMG] MyDocument.m arrange codes

--- MyDocument.m
+++ MyDocument.m
@@ -5,82 +5,11 @@
 
 @implementation MyDocument
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Add your subclass-specific initialization here.
-        // If an error occurs here, send a [self release] message and return nil.
-    }
-    return self;
-}
-
-- (void)windowControllerDidLoadNib:(NSWindowController *) aController
-{
-    [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
-}
-
-- (NSData *)dataRepresentationOfType:(NSString *)aType
-{
-    // Insert code here to write your document from the given data.  You can also choose to override -fileWrapperRepresentationOfType: or -writeToFile:ofType: instead.
-    return nil;
-}
-
-- (void)makeWindowControllers
-{
-#if useLog
-	NSLog(@"start makeWindowControlls");
-#endif	
-	targetWindowController = [[MyWindowController alloc] initWithWindowNibName:@"MyDocument"];
-    [self addWindowController:targetWindowController];
-}
-
+#pragma mark dmg tasks
 - (void)setFormatDict:(NSDictionary *)dictionary
 {
 	[dmgMaker setDmgFormat:[dictionary objectForKey:@"formatID"]];
 	[dmgMaker setDmgSuffix:[dictionary objectForKey:@"extension"]];	
-}
-
-- (DiskImageMaker *)dmgMaker
-{
-	return self->dmgMaker;
-}
-
-- (void)setDmgMaker:(DiskImageMaker *)theObject
-{
-	[theObject retain];
-	[dmgMaker release];
-	dmgMaker = theObject;
-}
-
-- (BOOL)readFromFile:(NSString *)filePath ofType:(NSString *)type
-{
-#if useLog
-	NSLog(@"start readFromFile");
-#endif
-	DiskImageMaker *dmgObj = [[DiskImageMaker alloc] initWithSourcePath:filePath];
-	[self setDmgMaker:[dmgObj autorelease]];
-	//NSLog(filePath);
-	
-    return YES;
-}
-
-- (void)dealloc
-{
-	//[[NSNotificationCenter defaultCenter] removeObserver:self];
-	[targetWindowController release];
-	[super dealloc];
-}
-
-- (void)setIsFirstDocument
-{
-	self->isFirstDocument = TRUE;
-}
-
-- (BOOL)isFirstDocument
-{
-	return self->isFirstDocument;
 }
 
 -(void) dmgDidTerminate:(NSNotification *) notification
@@ -99,17 +28,10 @@
 		NSString *theMessage = [dmgObj terminationMessage];
 		[targetWindowController showAlertMessage:NSLocalizedString(@"Error! Can't progress jobs.","") withInformativeText:theMessage];
 	}
+	
 	NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
 	[notificationCenter removeObserver:self];
 	[notificationCenter removeObserver:targetWindowController];
-}
-
-- (NSString *)updateTargetPathByFormatDict:(NSDictionary *)dictionary
-{
-	[dmgMaker setDmgFormat:[dictionary objectForKey:@"formatID"]];
-	[dmgMaker setDmgSuffix:[dictionary objectForKey:@"extension"]];
-	
-	return [dmgMaker dmgPath];
 }
 
 - (void)makeDmg
@@ -139,6 +61,78 @@
 			showAlertMessage:NSLocalizedString(@"Can't progress jobs.","")
 			withInformativeText:NSLocalizedString(@"Not enough free space for creating a disk image.", "")];
 	}
+}
+
+#pragma mark accessors
+- (DiskImageMaker *)dmgMaker
+{
+	return dmgMaker;
+}
+
+- (void)setDmgMaker:(DiskImageMaker *)theObject
+{
+	[theObject retain];
+	[dmgMaker release];
+	dmgMaker = theObject;
+}
+
+- (void)setIsFirstDocument
+{
+	isFirstDocument = TRUE;
+}
+
+- (BOOL)isFirstDocument
+{
+	return isFirstDocument;
+}
+
+#pragma mark init and dealloc
+- (void)dealloc
+{
+	[targetWindowController release];
+	[super dealloc];
+}
+
+#pragma mark overriding NSDocument
+- (void)windowControllerDidLoadNib:(NSWindowController *) aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
+- (NSData *)dataRepresentationOfType:(NSString *)aType
+{
+    // Insert code here to write your document from the given data.  You can also choose to override -fileWrapperRepresentationOfType: or -writeToFile:ofType: instead.
+    return nil;
+}
+
+- (BOOL)readFromFile:(NSString *)filePath ofType:(NSString *)type
+{
+#if useLog
+	NSLog(@"start readFromFile");
+#endif
+	DiskImageMaker *dmgObj = [[DiskImageMaker alloc] initWithSourcePath:filePath];
+	[self setDmgMaker:[dmgObj autorelease]];
+    return YES;
+}
+
+- (void)makeWindowControllers
+{
+#if useLog
+	NSLog(@"start makeWindowControlls");
+#endif	
+	targetWindowController = [[MyWindowController alloc] initWithWindowNibName:@"MyDocument"];
+    [self addWindowController:targetWindowController];
+}
+
+#pragma mark others
+
+- (NSString *)updateTargetPathByFormatDict:(NSDictionary *)dictionary
+{
+	[dmgMaker setDmgFormat:[dictionary objectForKey:@"formatID"]];
+	[dmgMaker setDmgSuffix:[dictionary objectForKey:@"extension"]];
+	
+	return [dmgMaker dmgPath];
 }
 
 - (BOOL)respondsToSelector:(SEL)aSelector
